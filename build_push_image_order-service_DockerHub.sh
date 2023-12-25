6a745d7ca124c3653d60b7e2885314502114036a
nrwahyuaji <nrwahyuaji@gmail.com>
2023-12-25 21:16:34 +0800

    edit build.sh on order-service

--- build_push_image_order-service_DockerHub.sh
+++ build_push_image_order-service_DockerHub.sh
@@ -1,7 +1,10 @@
 #! /bin/dash
 
+# Build docker image menggunakan berkas dari Docker Hub
 docker build -t ajik769/order-service:latest .
 
+# Login ke Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u ajik769 --password-stdin
 
+# Push image ke Docker Hub
 docker push ajik769/order-service:latest
